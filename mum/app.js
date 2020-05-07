21d73b9678f8a30fb3b9fcba6a570430ee7a500d
A01650434 <47993558+A01650434@users.noreply.github.com>
2020-05-07 12:05:31 -0500

    Update app.js

--- mum/app.js
+++ mum/app.js
@@ -33,41 +33,41 @@ btn.addEventListener('click', () =>{
 function readOutLoud(message){
     const speech = new SpeechSynthesisUtterance();
 
-    speech.text = 'cool, cool, cool, no entendí';
+    speech.text = 'oops';
 
-    if (message.includes('hola')){
+    if (message.includes('Hola')){
         const finalText = 
         greetings[Math.floor(Math.random() * greetings.length)];
         speech.text = finalText;
-    };
+    }
 
-    if (message.includes('que dia es hoy')){
+    if (message.includes('Qué día es hoy')){
         const finalText = 
         day[Math.floor(Math.random() * day.length)];
         speech.text = finalText;
-    };
+    }
 
-    if (message.includes('eschuchar mensajes')){
+    if (message.includes('Escuchar mensaje')){
         const finalText = 
         mail[Math.floor(Math.random()* mail.length)];
         speech.text = finalText;
-    };
+    }
 
     if (message.includes('mis frases populares')){
         const finalText = 
         quotes[Math.floor(Math.random()* quotes.length)];
-        speech.text = finalText;
-    }; 
+        speech.text = finalText; 
+    }
 
     if (message.includes('tengo')){
         speech.text = 'Sí, de: Reny, Florecita, Mamá, Javi, Dafne, Mariel, Hugo, Pazzi, Elisa';
-    };
+    }
 
-    if (message.includes('mariel')){
+    if (message.includes('Mariel')){
         speech.text = 'Feliz cumpleaños. Te amo';
     };
 
-    if (message.includes('reny')){
+    if (message.includes('Renata')){
         speech.text = 'Mamá, mamá... te amo';
     };
 
@@ -75,28 +75,28 @@ function readOutLoud(message){
         speech.text = 'Hermanita, te amo. Siempre serás mi brujula. Gracias por quedarte con nostras.';
     };
 
-    if (message.includes('mama')){
-        speech.text = 'Alecita , Mi Macky eres la estrella que me acompaña en este hermoso lugar que es vida , muchas gracias hija por favor existir .';
+    if (message.includes('mamá')){
+        speech.text = 'Alecita , Mi Macky eres la estrella que me acompaña en este hermoso lugar que es vida , muchas gracias hija por existir .';
     };
 
-    if (message.includes('javi')){
+    if (message.includes('Javier')){
         speech.text = 'que el universo y el tiempo confabulen con la tierra que a tus pies yace y la sigas colmando de bendiciones a través de tus sonrisas, feliz cumpleaños ';
     };
 
-    if (message.includes('dafne')){
+    if (message.includes('hija')){
         speech.text = 'Mami te amo, gracias por creecer conmigo. Feliz cumpleaños';
     };
 
-    if (message.includes('hugo')){
+    if (message.includes('Hugo')){
         speech.text = 'Mia. Hermana te quiero mucho  pasatela super';
     };
 
-    if (message.includes('elisa')){
+    if (message.includes('Elisa')){
         speech.text = 'Ale , mi hermana mayor, mi ejemplo de amor, te quiero mucho ';
     };
 
-    if (message.includes('pazzi')){
-        speech.text = '';
+    if (message.includes('huguito')){
+        speech.text = 'feliz cumpleaños tia maky y cantarle las mañanitas';
     };
 
     speech.volume = 0.5;
